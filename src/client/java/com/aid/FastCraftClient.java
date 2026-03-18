3a29783f057839de2c26590d912ffb92aa169c30
ilaaleksandrovskij7-oss <ilaaleksandrovskij7@gmail.com>
2026-03-18 17:03:13 +0300

    new
    lol

---
+++ src/client/java/com/aid/FastCraftClient.java
@@ -0,0 +1,10 @@
+package com.aid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FastCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
