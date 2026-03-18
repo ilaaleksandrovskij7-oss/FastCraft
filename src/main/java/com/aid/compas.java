942e519be66a7a24ceafefc09783029943c1ed79
Aid <ilaaleksandrovskij7@gmail.com>
2026-03-18 21:02:13 +0300

    Update compas.java

--- src/main/java/com/aid/compas.java
+++ src/main/java/com/aid/compas.java
@@ -5,6 +5,6 @@ import net.minecraft.world.entity.player.Player;
 
 public class compas {
     public static void compasw(Player player){
-        player.displayClientMessage(Component.literal("X: " + player.getX() + " Y: " + player.getY() + " Z: " + player.getZ() + " :3"), true);
+        player.displayClientMessage(Component.literal("§1 X: " + (int) player.getX() + " Y: " + (int) player.getY() + " Z: " + (int) player.getZ() + " :3"), true);
     }
 }
