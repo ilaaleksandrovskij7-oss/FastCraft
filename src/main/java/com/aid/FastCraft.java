4aeaa97a4b13c1b0d22b7042a6acfe28a3fc2848
ilaaleksandrovskij7-oss <ilaaleksandrovskij7@gmail.com>
2026-03-18 17:10:00 +0300

    Update FastCraft.java

--- src/main/java/com/aid/FastCraft.java
+++ src/main/java/com/aid/FastCraft.java
@@ -2,23 +2,25 @@ package com.aid;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FastCraft implements ModInitializer {
 	public static final String MOD_ID = "fastcraft";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+
 
 		LOGGER.info("Hello Fabric world!");
+
+		ServerEntity
 	}
 }
